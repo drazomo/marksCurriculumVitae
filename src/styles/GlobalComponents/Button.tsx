4d1ba544bc6e15308127b27e36df36fed2cf3643
Mark Rasavong <rasavong.mark@gmail.com>
2022-10-25 20:19:09 +0200

    add: Universal Button component to use for site

---
+++ src/styles/GlobalComponents/Button.tsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { ButtonBack, ButtonFront } from '.'
+
+interface ButtonProps {
+	children: React.ReactNode
+	onClick: (event: MouseEvent) => void
+	alt: boolean
+	form: boolean
+	disabled: boolean
+}
+
+const Button = ({
+	children,
+	alt,
+	form,
+	disabled,
+	onClick,
+}: Partial<ButtonProps>) => {
+	return (
+		<ButtonBack alt={alt} form={form} disabled={disabled}>
+			{children}
+			<ButtonFront alt={alt} onClick={() => onClick} disabled={disabled}>
+				{children}
+			</ButtonFront>
+		</ButtonBack>
+	)
+}
+
+export default Button
